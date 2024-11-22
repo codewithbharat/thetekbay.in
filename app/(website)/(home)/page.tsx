338c266f8ba064a6518ca9c002a15ce8ad31fe9c
Bharat Ranjan <ranjanbharat54@gamil.com>
2024-11-22 22:51:43 +0530

    BLogPostCard

--- app/(website)/(home)/page.tsx
+++ app/(website)/(home)/page.tsx
@@ -1,32 +1,80 @@
-import React from 'react'
+import BlogPostCard from "@/components/web/BlogPostCard";
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-       <section className='my-2'>
-        {/* breaking news label */}
-        <h2 className='border-b-4 font-serif text-xl font-medium'>Breaking News</h2>
-        {/* image */}
-        <div className="w-full bg-gray-400 my-2" style={{ height: 'calc(100vw * 9 / 16)' }}></div>
+      <section className="flex flex-col md:flex-row">
+        {/* breaking News  */}
+        <section className="my-2  mx-2 sm:basis-1 md:basis-3/6">
+          {/* breaking news label */}
+          <h2 className="border-b-4 font-serif text-xl py-1 font-medium">
+            Breaking News
+          </h2>
+          <BlogPostCard
+            date="20 Nov 2024"
+            category="Technology"
+            author="John Doe"
+            title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies metus. Mauris in quam id turpis aliquet aliquet."
+            link="/blog/1"
+            titleFontSize="text-lg"
+          />
+        </section>
 
-        {/* date, category and author */}
-        <div className="flex items-center mt-2 font-serif">
-          <span className="text-gray-600">{new Date().toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</span>
-          <span className="text-gray-600 mx-2">|</span>
-          <span className="text-gray-600">Technology</span>
-          <span className="text-gray-600 mx-2">|</span>
-          <span className="text-gray-600">John Doe</span>
-        </div>
-        {/* title  */}
-        <h3 className='text-2xl mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
-        {/* short description with readm more button */}
-        <p className='text-gray-600 mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat,
-          vestibulum mi nec, ultricies metus. Mauris in quam id turpis aliquet aliquet. </p>
-        <button className='bg-gray-600 text-white px-4 py-2 mt-2'>Read More</button>
-
+        {/* latest News  */}
+        <section className="my-2 mx-2 sm:basis-1 md:basis-3/6">
+          {/* latest news label */}
+          <h2 className="border-b-4 font-serif text-xl py-1 font-medium">
+            Latest News
+          </h2>
+          <BlogPostCard
+            date="20 Nov 2024"
+            category="Technology"
+            author="John Doe"
+            title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi nec, ultricies metus. Mauris in quam id turpis aliquet aliquet."
+            link="/blog/1"
+            showAuthor={false}
+            showShortDescription={false}
+            showImage={false}
+            titleFontSize="text-sm"
+          />
+          <BlogPostCard
+            date="19 Nov 2024"
+            category="Health"
+            author="Jane Smith"
+            title="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            link="/blog/2"
+            showAuthor={false}
+            showShortDescription={false}
+            showImage={false}
+            titleFontSize="text-sm"
+          />
+          <BlogPostCard
+            date="18 Nov 2024"
+            category="Science"
+            author="Alice Johnson"
+            title="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+            link="/blog/3"
+            showAuthor={false}
+            showShortDescription={false}
+            showImage={false}
+            titleFontSize="text-sm"
+          />
+          <BlogPostCard
+            date="17 Nov 2024"
+            category="Business"
+            author="Michael Brown"
+            title="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            link="/blog/4"
+            showAuthor={false}
+            showShortDescription={false}
+            showImage={false}
+            titleFontSize="text-sm"
+          />
+        </section>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/web/BlogPostCard.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import React from "react";
+
+interface BlogPostCardProps {
+  date: string;
+  category: string;
+  author: string;
+  title: string;
+  link: string;
+  titleFontSize?: string;
+  showImage?: boolean;
+  showAuthor?: boolean;
+  showShortDescription?: boolean;
+}
+
+const BlogPostCard = ({
+  date,
+  category,
+  author,
+  title,
+  link,
+  titleFontSize,
+  showAuthor = true,
+  showImage = true,
+  showShortDescription = true,
+}: BlogPostCardProps) => {
+  return (
+    <div className="m-10">
+      {/* image */}
+      {showImage && <div className="w-full aspect-video bg-gray-200"></div>}
+
+      {/* date, category and author */}
+      <div className="flex items-center mt-2 font-serif">
+        <span className="text-gray-600">{date}</span>
+        <span className="text-gray-600 mx-2">|</span>
+        <span className="text-gray-600">{category}</span>
+        {showAuthor && (
+          <>
+            <span className="text-gray-600 mx-2">|</span>
+            <span className="text-gray-600">{author}</span>
+          </>
+        )}
+      </div>
+      {/* title  */}
+      <h3 className={`${titleFontSize ? titleFontSize : "text-2xl"} mt-2`}>
+        {title}
+      </h3>
+      {/* short description with readm more button */}
+      {showShortDescription && (
+        <p className="text-gray-600 mt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
+          purus feugiat, vestibulum mi nec, ultricies metus. Mauris in quam id
+          turpis aliquet aliquet.{" "}
+        </p>
+      )}
+      <Link href={link} className="text-gray-800 font-semibold mt-2">
+        Read More
+      </Link>
+    </div>
+  );
+};
+
+export default BlogPostCard;
