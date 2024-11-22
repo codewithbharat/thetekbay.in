0ce598ae75790f4db7d8efa4bf2b3e53e12742ab
Bharat Ranjan <ranjanbharat54@gamil.com>
2024-11-22 18:18:29 +0530

    breaking News Component

--- app/(website)/(home)/page.tsx
+++ app/(website)/(home)/page.tsx
@@ -3,7 +3,28 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home page
+       <section className='my-2'>
+        {/* breaking news label */}
+        <h2 className='border-b-4 font-serif text-xl font-medium'>Breaking News</h2>
+        {/* image */}
+        <div className="w-full bg-gray-400 my-2" style={{ height: 'calc(100vw * 9 / 16)' }}></div>
+
+        {/* date, category and author */}
+        <div className="flex items-center mt-2 font-serif">
+          <span className="text-gray-600">{new Date().toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</span>
+          <span className="text-gray-600 mx-2">|</span>
+          <span className="text-gray-600">Technology</span>
+          <span className="text-gray-600 mx-2">|</span>
+          <span className="text-gray-600">John Doe</span>
+        </div>
+        {/* title  */}
+        <h3 className='text-2xl mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
+        {/* short description with readm more button */}
+        <p className='text-gray-600 mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat,
+          vestibulum mi nec, ultricies metus. Mauris in quam id turpis aliquet aliquet. </p>
+        <button className='bg-gray-600 text-white px-4 py-2 mt-2'>Read More</button>
+
+      </section>
     </div>
   )
 }
--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,31 +1,37 @@
-import React, { ReactNode } from 'react';
-import Link from 'next/link';
+import React, { ReactNode } from "react";
+import Link from "next/link";
 
 interface LayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-    return (
-        <div>
-             {/* the header title  */}
-            <header className="text-center text-6xl font-bold p-4 font-serif uppercase">
-               The TechBay
-            </header>
-            <nav className="flex gap-4 p-4 justify-center border-b-2 border-b-black mx-10 border-t-4 font-medium my-2">
-                <Link href="/" className="hover:text-gray-400">
-                    Home
-                </Link>
-                <Link href="/about" className="hover:text-gray-400">
-                    About
-                </Link>
-                <Link href="/contact" className="hover:text-gray-400">
-                    Contact
-                </Link>
-            </nav>
-            <main className='mx-10'>{children}</main>
-        </div>
-    );
+  return (
+    <div>
+      {/* the header title  */}
+      <header className="text-center text-6xl font-bold p-4 font-serif captalize">
+        <Link href="/"> The TechBay</Link>
+        {/* date today  */}
+      </header>
+        <p className="text-sm mx-10"> {new Date().toDateString()} </p>
+      <nav className="flex gap-4 p-4 justify-center border-b-2 border-b-black mx-10 border-t-4 font-medium">
+        <Link href="/" className="hover:text-gray-400">
+          Home
+        </Link>
+        <Link href="/blog" className="hover:text-gray-400">
+          Blog
+        </Link>
+        <Link href="/about" className="hover:text-gray-400">
+          About
+        </Link>
+
+        <Link href="/contact" className="hover:text-gray-400">
+          Contact
+        </Link>
+      </nav>
+      <main className="mx-10">{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
